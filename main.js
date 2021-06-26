3ebde2877e6d68eab2d5868041af3fbc966fa705
Daniel <kr4usdc@gmail.com>
2021-06-26 11:25:11 -0300

    version 1

---
+++ main.js
@@ -0,0 +1,18 @@
+const links = document.querySelectorAll('.nav-link')
+
+Array.prototype.forEach.call(links, function(elem, index) {
+    //Get the hyperlink target and if it refers to an id go inside condition
+    var elemAttr = elem.getAttribute("href");
+    if(elemAttr && elemAttr.includes("#")) {
+      //Replace the regular action with a scrolling to target on click
+      elem.addEventListener("click", function(ev) {
+        ev.preventDefault();
+        //Scroll to the target element using replace() and regex to find the href's target id
+        document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+            });
+      });
+    }
+  });
