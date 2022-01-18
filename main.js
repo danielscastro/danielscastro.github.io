52bc6b1fc380ed6f975d3dd563a6ac25852306cb
Daniel Castro <danielscastro@protonmail.com>
2022-01-17 22:37:05 -0300

    refactoring and increasing projects

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
+//Smooth scroll code
 const links = document.querySelectorAll('.nav-link')
 
 Array.prototype.forEach.call(links, function(elem, index) {
-    //Get the hyperlink target and if it refers to an id go inside condition
     var elemAttr = elem.getAttribute("href");
     if(elemAttr && elemAttr.includes("#")) {
-      //Replace the regular action with a scrolling to target on click
       elem.addEventListener("click", function(ev) {
         ev.preventDefault();
-        //Scroll to the target element using replace() and regex to find the href's target id
         document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
             behavior: "smooth",
             block: "start",
@@ -16,3 +14,65 @@ Array.prototype.forEach.call(links, function(elem, index) {
       });
     }
   });
+
+//populate project cards 
+const portfolioHeader = document.querySelector('[data-js="portfolio-header"]');
+
+const projects = [{
+  name: 'Todo App',
+  thumbnail: './assets/todo-app.png',
+  deployedLink: 'https://to-do-app-lyart-chi.vercel.app',
+  text: 'Um ToDo App simples, porém com alguns temperos interessantes para distanciá-lo dos mais comuns: é possível não só adicionar e remover tarefas, como também riscá-las (tarefa completa) e filtrá-las. Também utilizei o localStorage para armazenar as mudanças no navegador.'
+},
+{
+  name: 'Projeto FAQ usando acordeão',
+  thumbnail: './assets/accordion-faq.png',
+  deployedLink: 'https://faq-accordion-card-2wj064ixa.vercel.app',
+  text: 'Um desafio proposto pelo <a href="https://www.frontendmentor.io" rel="noopener" target="_blank">FrontEnd Mentor</a> de site estático, porém utilizando os acordeões para mostrar e esconder as respostas das perguntas. Feito em design responsivo para a maior parte das telas.'
+},
+{
+  name: 'Encurtador de Links',
+  thumbnail: './assets/url-shortener.png',
+  deployedLink: 'https://url-shortener-api-bykopieqg-gdankas.vercel.app',
+  text: 'Um desafio proposto <a href="https://www.frontendmentor.io" rel="noopener" target="_blank">FrontEnd Mentor</a>. O usuário alimenta a aplicação com uma URL válida, a mesma consulta uma API de encurtamento de links e responde com o link original e o encurtado.'
+},
+{
+  name: 'Weather Web App',
+  thumbnail: './assets/city-weather-app.png',
+  deployedLink: 'https://city-weather-app-drab.vercel.app',
+  text: 'Pequena aplicação que consulta a API do AccuWeather e responde com a temperatura atual da cidade pesquisada, com alguma previsão básica do tempo.'
+},
+{
+  name: 'Currency Converter App',
+  thumbnail: './assets/currency-converter.png',
+  deployedLink: 'https://currency-converter-app-eight.vercel.app',
+  text: 'Este aplicativo itera sobre três moedas mundiais (escalável) e torna possível fazer uma conversão em tempo real de uma moeda para outra. É possível graças à API do Exchangerate.'
+}
+]
+
+const populatePortfolio = portfolioCardTemplate => {
+  projects.forEach(({ name, thumbnail, deployedLink, text }) => {
+    portfolioCardTemplate.innerHTML += `
+    <div class="card bg-dark text-white mb-3">
+      <a href="${thumbnail}">
+        <img height="250px" class="card-img-top" src="${thumbnail}" alt="${name}" loading="lazy">
+      </a>
+    <div class="card-body">
+      <a class="link text-white text-bold" href="${deployedLink}" target="_blank" rel="noopener">
+        <h5 class="card-title text-white">${name}</h5>
+      </a>
+      <p class="card-text">${text}</p>
+    </div>
+  `;
+  })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const portfolioCardTemplate = document.createElement('div');
+  portfolioCardTemplate.classList.add('mt-4', 'card-deck');
+  portfolioCardTemplate.style="font-size: 10";
+
+  populatePortfolio(portfolioCardTemplate);
+  
+  portfolioHeader.insertAdjacentElement('afterend', portfolioCardTemplate)
+})
